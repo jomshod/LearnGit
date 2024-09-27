a5544c4ee4e78555b25c839999d7d3bd552f55b4
LINK COMPUTER <aeoikhlaspurw@gmail.com>
2024-09-27 07:47:06 +0500

    added the app composable

--- app/src/main/java/com/jimmy/learngit/MainActivity.kt
+++ app/src/main/java/com/jimmy/learngit/MainActivity.kt
@@ -16,32 +16,15 @@ import com.jimmy.learngit.ui.theme.LearnGitTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             LearnGitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+             MyApp()
                 }
             }
         }
     }
-}
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LearnGitTheme {
-        Greeting("Android")
-    }
+fun MyApp(modifier: Modifier = Modifier) {
+    Text("hello user")
 }
